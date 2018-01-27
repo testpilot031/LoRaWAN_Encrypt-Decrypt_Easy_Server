da5b62bd56b164cf1ad9f1538cec00cbce55e142
kou <testpilot031@gmail.com>
2018-01-27 22:30:04 +0900

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+#利用するUbuntuのイメージ
+FROM ubuntu:16.04
+
+# インストールpython3-pip wget lsof apache2 apache2-dev(pip3 install mod_wsgiで必要)
+RUN set -x && \
+    apt-get update && \
+    apt-get install -y vim git sudo openssh-server mlocate lsof python3 python3-pip python-dev python3-dev libffi-dev libssl-dev apache2 apache2-dev && \
+    pip3 install --upgrade pip && \
+    #pip3 install virtualenv && \
+	pip install mod_wsgi && \
+	pip3 install falcon && \
+    pip3 install python-lora
+RUN useradd ubuntu && \
+    echo 'ubuntu:password' |chpasswd && \
+    gpasswd -a ubuntu sudo && \
+	mkdir /home/ubuntu/ && \
+    chmod 755 /home/ubuntu/ && \
+	mkdir /home/ubuntu/script/ && \
+	chmod 755 /home/ubuntu/script/ 
+ADD python.conf /etc/apache2/sites-available/
+RUN a2ensite python
+ADD app.wsgi /home/ubuntu/script/
+RUN chmod 755 /home/ubuntu/script/app.wsgi
+ADD main.py /home/ubuntu/script/
+RUN chmod 755 /home/ubuntu/script/main.py
+RUN updatedb
+#最後の方http://kimh.github.io/blog/jp/docker/gothas-in-writing-dockerfile-jp/#treat_your_container_like_a_binary_with_cmd
+ENTRYPOINT service ssh start && service apache2 start && tail -f /dev/null
+
+<!--stackedit_data:
+eyJoaXN0b3J5IjpbMTkzNDExMjc0N119
+-->
---
+++ main.py
@@ -0,0 +1,74 @@
+#!/home/kaisen/decryptLorawan/env/bin/python3
+# coding: utf-8
+import json
+import falcon
+import re
+from lora.crypto import loramac_decrypt
+class Extends():
+    def before_resource(req, resp, resource, param):
+        print('Content-type: application/json\n')
+        print('Cache-Control: private\n')
+@falcon.before(Extends.before_resource)
+class DecryptAndEncryptData(object):
+    def on_get(self,req,resp):
+        try:
+            payload = str(req.get_param('payload'))
+            #print('Content-type: application/json\n')
+            #print('Cache-Control: private\n')
+            fcnt = int(req.get_param('fcnt'),10)
+            key = str(req.get_param('key'))
+            dev_addr = str(req.get_param('dev_addr'))
+            direction = str(req.get_param('direction'))
+            intDir = 0
+            if direction == "UP":
+                intDir = 0
+            elif direction == "DOWN":
+                intDir = 1
+            resp.body = json.dumps(loramac_decrypt(payload, fcnt, key, dev_addr, intDir))
+        except Exception as e:
+            print '=== error ==='
+            print 'type:' + str(type(e))
+            print 'args:' + str(e.args)
+            print 'message:' + e.message
+            print 'e自身:' + str(e)
+    def on_post(self, req, resp):
+        body = req.stream.read()
+        body = body.decode("utf-8")
+        # data = json.dumps(body)
+        data = json.loads(body)
+        intDir = 0
+        if data['direction'] == "UP":
+            intDir = 0
+        elif data['direction'] == "DOWN":
+            intDir = 1
+        resp.body = json.dumps(loramac_decrypt(str(data['payload']), int(data['fcnt'], 10), str(data['key']), str(data['dev_adder']), intDir))
+        
+class TypeUtil():
+    def ishex(string):
+        pattern = '^[0-9A-Fa-f]+$'
+        reply = ''
+        if not str(re.match(pattern, string)) == 'None':
+            reply = 'true'
+        else:
+            reply = 'false'
+        return reply
+    def convHexToBd(hexdata):
+        print(hexdata)
+        if not self.ishex(hexdata) :
+            return 'not hex'
+        bddata = ""
+        for char in hexdata:
+            bddata += format(int(char, 16), '04b')
+        return bddata
+#class TypeCheck():
+#   def isint(obj):
+        
+#   def
+        
+        
+        
+        
+application = falcon.API()
+application.add_route("/",DecryptAndEncryptData())
+application.add_route("/decrypt/",DecryptAndEncryptData())
+application.add_route("/encrypt/",DecryptAndEncryptData())
